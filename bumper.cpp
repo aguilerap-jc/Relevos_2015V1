6d83d6c4513b02e9610cfc5ddb5802ba9fb2bccc
AguilerapJC <aguilerap.jc@gmail.com>
2015-11-26 06:27:15 -0600

    First Commit localLastNAO

---
+++ bumper.cpp
@@ -0,0 +1,189 @@
+/**
+ * Copyright (c) 2011 Aldebaran Robotics
+ */
+
+#include "bumper.h"
+#include <time.h>
+#include <alvalue/alvalue.h>
+#include <alcommon/alproxy.h>
+#include <alcommon/albroker.h>
+#include <qi/log.hpp>
+#include <althread/alcriticalsection.h>
+
+Bumper::Bumper(boost::shared_ptr<AL::ALBroker> broker, const std::string& name): AL::ALModule(broker, name),fCallbackMutex(AL::ALMutex::createALMutex()),naoVision(true), naoMovement(true)
+{
+  start = true;
+  setModuleDescription("This module presents how to subscribe to a simple event (here RightBumperPressed) and use a callback method.");
+  functionName("onRightBumperPressed", getName(), "Method called when the right bumper is pressed. Makes a LED animation.");
+  BIND_METHOD(Bumper::onRightBumperPressed);
+
+  functionName("onLeftBumperPressed", getName(), "Method called when the right bumper is pressed. Makes a LED animation.");
+  BIND_METHOD(Bumper::onLeftBumperPressed);
+}
+
+Bumper::~Bumper() {
+  fMemoryProxy.unsubscribeToEvent("onRightBumperPressed", "Bumper");
+  fMemoryProxy2.unsubscribeToEvent("onLeftBumperPressed","Bumper");
+}
+
+void Bumper::init() {
+  try {
+    /** Create a proxy to ALMemory.
+    */
+    fMemoryProxy = AL::ALMemoryProxy(getParentBroker());
+    fMemoryProxy2 = AL::ALMemoryProxy(getParentBroker());
+
+    fState = fMemoryProxy.getData("RightBumperPressed");
+    fState2 = fMemoryProxy2.getData("LeftBumperPressed");
+    /** Subscribe to event LeftBumperPressed
+    * Arguments:
+    * - name of the event
+    * - name of the module to be called for the callback
+    * - name of the bound method to be called on event
+    */
+    fMemoryProxy.subscribeToEvent("RightBumperPressed", "Bumper","onRightBumperPressed");
+    fMemoryProxy2.subscribeToEvent("LeftBumperPressed", "Bumper","onLeftBumperPressed");
+  }
+  catch (const AL::ALError& e) {
+    qiLogError("module.example") << e.what() << std::endl;
+  }
+}
+
+void Bumper::onRightBumperPressed() {
+  qiLogInfo("module.example") << "Executing callback method on right bumper event" << std::endl;
+  /**
+  * As long as this is defined, the code is thread-safe.
+  */
+  AL::ALCriticalSection section(fCallbackMutex);
+
+  /**
+  * Check that the bumper is pressed.
+  */
+  fState =  fMemoryProxy.getData("RightBumperPressed");
+  if (fState  > 0.5f) {
+    return;
+  }
+  try {
+    fTtsProxy = AL::ALTextToSpeechProxy(getParentBroker());
+    //fTtsProxy.say("Right bumper pressed");
+    if (start) 
+      walkWithBumper();
+  }
+  catch (const AL::ALError& e) {
+    qiLogError("module.example") << e.what() << std::endl;
+  }
+}
+
+
+void Bumper::onLeftBumperPressed() {
+  qiLogInfo("module.example") << "Executing callback method on right bumper event" << std::endl;
+  /**
+  * As long as this is defined, the code is thread-safe.
+  */
+  AL::ALCriticalSection section(fCallbackMutex);
+
+  /**
+  * Check that the bumper is pressed.
+  */
+  fState2 =  fMemoryProxy2.getData("LeftBumperPressed");
+  if (fState2  > 0.5f) {
+    return;
+  }
+  try {
+    fTtsProxy2 = AL::ALTextToSpeechProxy(getParentBroker());
+    //fTtsProxy2.say("Left bumper pressed");
+    if (!start)
+      emergencyStop();
+  }
+  catch (const AL::ALError& e) {
+    qiLogError("module.example") << e.what() << std::endl;
+  }
+}
+
+void Bumper::walkWithBumper(){
+    start = false;
+    bool LOCAL = true;         // Flap for the kind of execution (local or remote).
+    bool NAO = true;
+    bool finish = false;
+    char key = 'x';
+    double angleToBlackLine;    // Angle of the detected line.
+    int yellowArea;
+    int finalArea;
+    bool naoDetected = false; 
+    Mat bottomImage, topImage;
+    NaoVision naoVision(LOCAL);
+    NaoMovement naoMovement(LOCAL);
+    VideoCapture cap(1);        // Class for video capturing from video files or cameras.
+
+    naoMovement.initialPositionRelayRace();
+
+    while (!naoDetected){
+        if (NAO) {
+            topImage = naoVision.getImageFrom(NaoVision::TOP_CAMERA);
+        } else {
+            cap >> topImage;
+            naoVision.setSourceMat(topImage);
+        }
+
+        yellowArea = naoVision.getAreaYellowColor(topImage);
+        cout << "Yellow Area : " << yellowArea << endl;
+        if(yellowArea > 10 && yellowArea < 40){
+            cout << "Brace Your selfs" << endl;
+            cout<<"Yellow Area : " << yellowArea << endl;
+            naoDetected = true;
+        }
+
+            for (int i = 0; i < 250000; i++);   // Delay.
+    }
+
+        naoMovement.initialPositionIndividualRace();
+
+    while (!finish) {
+        if (NAO) {
+            bottomImage = naoVision.getImageFrom(NaoVision::BOTTOM_CAMERA);
+
+        } else {
+            cap >> bottomImage;
+            naoVision.setSourceMat(bottomImage);
+        }
+
+        if (naoVision.naoIsNearTheGoalRelayRace(bottomImage)) {
+            topImage = naoVision.getImageFrom(NaoVision::TOP_CAMERA);
+
+            yellowArea = naoVision.getAreaYellowColor(topImage);
+            cout << " Yellow Area : " << yellowArea << endl;
+            finalArea = naoVision.FinalLineFilterRelayRace(bottomImage);
+            cout << "Final Area : " << finalArea << endl;
+
+            if(yellowArea >=12 && yellowArea< 40){
+                cout<<"Finishing on left" << endl;
+                naoMovement.leftCorrection();
+            }
+            else if (yellowArea >= 2) {
+                cout<<"Finishing on middle" << endl;
+                naoMovement.middleCorrection();
+            }
+            else {
+                cout<<"Finishing on right" << endl;
+                naoMovement.rightCorrection();
+            }
+            finish = true;
+
+        } else {
+            angleToBlackLine = naoVision.calculateAngleToBlackLine();
+            naoMovement.moveInIndividualRace(angleToBlackLine);
+        }
+
+        for (int i = 0; i < 250000; i++);   // Delay.
+    }
+
+    naoVision.unsubscribe();
+    naoMovement.stop();
+}
+
+void Bumper::emergencyStop(){
+  fTtsProxy.say("On Stop");
+    naoVision.unsubscribe();
+    naoMovement.stop();
+    start = true;
+}
